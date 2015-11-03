bb2e612ec89d923f8d9a91ebfa01a1ac11eba158
Kyle Rabago-Banjo <krabagobanjo@outlook.com>
2015-11-03 09:05:35 -0500

    stuuuuubs!

---
+++ src/RxP.py
@@ -0,0 +1,12 @@
+import socket
+import argparse
+import hashlib
+import random
+import string
+import math
+
+class RxP(Object):
+    def __init__(self):
+        blah = ""
+
+    def create_socket():
